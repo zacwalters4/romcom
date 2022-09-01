bdef7a05bf3bbea86c85eedd1be9bfad1396f01a
Angie Staffieri <angie.staffieri@gmail.com>
2022-09-01 16:34:43 -0600

    Update the DOM and data model with random cover

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ var coverTitle = document.querySelector('.cover-title')
 var coverTagline1 = document.querySelector('.tagline-1')
 var coverTagline2 = document.querySelector('.tagline-2')
 var showRandomButton = document.querySelector('.random-cover-button')
-showRandom()
+var makeNewButton = document.querySelector('.make-new-button')
+var mainCover = document.querySelector('.main-cover')
+var makeNewForm = document.querySelector('.form-view')
 
 
 // We've provided a few variables below
@@ -16,17 +18,39 @@ var currentCover;
 
 // Add your event listeners here 👇
 showRandomButton.addEventListener('click', showRandom)
+// makeNewButton.addEventListener('click', makeNew)
+
+
 // // Create your event handlers and other functions here 👇
+
 function showRandom() {
-  coverImage.src = covers[getRandomIndex(covers)]
-  coverTitle.innerText = titles[getRandomIndex(titles)]
-  coverTagline1.innerText = descriptors[getRandomIndex(descriptors)]
-  coverTagline2.innerText = descriptors[getRandomIndex(descriptors)]
-  console.log(coverImage)
-  console.log(coverTitle)
-  console.log(coverTagline1)
-  console.log(coverTagline2)
+  var coverArray = [
+  covers[getRandomIndex(covers)],
+  titles[getRandomIndex(titles)],
+  descriptors[getRandomIndex(descriptors)],
+  descriptors[getRandomIndex(descriptors)]
+]
+  var randomCover = new Cover(
+    coverArray[0],
+    coverArray[1],
+    coverArray[2],
+    coverArray[3]
+  )
+  coverImage.src = coverArray[0]
+  coverTitle.innerText = coverArray[1]
+  coverTagline1.innerText = coverArray[2]
+  coverTagline2.innerText = coverArray[3]
+  console.log(randomCover)
+  return randomCover;
 }
+
+
+
+
+  // mainCover.style.visibility = 'hidden'
+  // makeNewForm.style.visibility = 'visible'
+  // document.getElementbyClassName('.main-cover').hidden = true;
+  // document.getElementbyClassName('.view form-view').hidden = false;
 
 
 
@@ -34,3 +58,4 @@ function showRandom() {
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+showRandom()
